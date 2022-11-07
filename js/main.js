009ed8821986863dd2b2beee805fe76f89348275
Emanuela91 <emanuela.marson91@gmail.com>
2022-11-07 17:22:29 +0100

    aggiunti in js costi biglietti: normali, minorenni, anziani

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@
 // Variabili e costanti
 let chilometri, eta, costoBiglietto, costoBigliettoMin, costoBigliettoAnz;
 const costoKm = 0.21;
-const minorenni = 17;
+const minorenni = 18;
 const anziano = 65;
 
 // Percorrenza in Km 
@@ -24,12 +24,21 @@ console.log(eta);
 // Costo del biglietto, somma Km e costo al Km
 costoBiglietto = chilometri * costoKm;
 
-
 console.log(costoBiglietto);
 
+// Costo costo biglietto minorenni e anziani
+costoBigliettoMin = ((chilometri * costoKm) * 0.8);
+costoBigliettoAnz = ((chilometri * costoKm) * 0.6);
 
 // Costo del biglietto con sconto
 // Minorenne o Anziano
-if(eta > minorenni ) {
-
+if(eta >= minorenni && eta < anziano){
+    document.getElementById("title").innerHTML = `Il costo del tuo biglietto è ${costoBiglietto}`;
+} else if(eta < minorenni){
+    document.getElementById("title").innerHTML = `Il costo del tuo biglietto è ${costoBigliettoMin}`;
+} else if(eta >= anziano){
+    document.getElementById("title").innerHTML = `Il costo del tuo biglietto è ${costoBigliettoAnz}`;
 }
+
+// Riduzione decimali con Parsfloat
+(costoBiglietto.toFixed(1));
